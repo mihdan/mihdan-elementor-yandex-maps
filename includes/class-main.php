0dbccb119acddda8d9e5fa3c440f333856c8b5b1
Mikhail Kobzarev <mihdan@yandex.ru>
2019-07-13 00:34:28 +0300

    WPCS для класса `Main`

--- includes/class-main.php
+++ includes/class-main.php
@@ -1,9 +1,18 @@
 <?php
+/**
+ * Main class of the plugin.
+ *
+ * @package mihdan-elementor-yandex-maps
+ */
+
 namespace MihdanElementorYandexMaps;
 
 use \Elementor\Settings;
 use \Elementor\Plugin;
 
+/**
+ * Class Main
+ */
 class Main {
 
 	/**
@@ -18,6 +27,8 @@ class Main {
 	private static $_instance = null;
 
 	/**
+	 * Yandex Map API key
+	 *
 	 * @var string $api_key Key for Yandex Maps API.
 	 */
 	private $api_key;
@@ -67,11 +78,11 @@ class Main {
 		$this->api_key = get_option( 'elementor_mihdan_elementor_yandex_maps_key' );
 
 		// Если не задан API ключ для карт.
-		if ( '' == $this->api_key ) {
+		if ( '' === $this->api_key ) {
 			add_action( 'admin_notices', [ $this, 'admin_notice_invalid_api_key' ] );
 		}
 
-		// Add Plugin actions
+		// Add Plugin actions.
 		add_action( 'elementor/init', [ $this, 'register_category' ] );
 		add_action( 'elementor/admin/after_create_settings/elementor', [ $this, 'register_settings' ] );
 		add_action( 'elementor/editor/before_enqueue_scripts', [ $this, 'editor_scripts' ] );
@@ -84,7 +95,7 @@ class Main {
 	 * Enqueue scripts for editor.
 	 */
 	public function editor_scripts() {
-		wp_enqueue_style( 'mihdan-elementor-yandex-maps-admin', plugins_url( '/frontend/css/mihdan-elementor-yandex-maps-admin.css', MIHDAN_ELEMENTOR_YANDEX_MAPS_FILE ) );
+		wp_enqueue_style( 'mihdan-elementor-yandex-maps-admin', plugins_url( '/frontend/css/mihdan-elementor-yandex-maps-admin.css', MIHDAN_ELEMENTOR_YANDEX_MAPS_FILE ), [], MIHDAN_ELEMENTOR_YANDEX_MAPS_VERSION );
 		wp_enqueue_script( 'mihdan-elementor-yandex-maps-api-admin', 'https://api-maps.yandex.ru/2.1/?lang=ru_RU&source=admin&apikey=' . $this->api_key, [ 'jquery' ], MIHDAN_ELEMENTOR_YANDEX_MAPS_VERSION, true );
 		wp_localize_script( 'mihdan-elementor-yandex-maps-api-admin', 'EB_WP_URL', array( 'plugin_url' => plugin_dir_url( __FILE__ ) ) );
 		wp_enqueue_script( 'mihdan-elementor-yandex-maps-admin', plugins_url( '/frontend/js/mihdan-elementor-yandex-maps-admin.js', MIHDAN_ELEMENTOR_YANDEX_MAPS_FILE ), [ 'mihdan-elementor-yandex-maps-api-admin' ], MIHDAN_ELEMENTOR_YANDEX_MAPS_VERSION, true );
@@ -94,7 +105,7 @@ class Main {
 	 * Enqueue styles for frontend.
 	 */
 	public function frontend_styles() {
-		wp_enqueue_style( 'mihdan-elementor-yandex-maps', plugins_url( '/frontend/css/mihdan-elementor-yandex-maps.css', MIHDAN_ELEMENTOR_YANDEX_MAPS_FILE ) );
+		wp_enqueue_style( 'mihdan-elementor-yandex-maps', plugins_url( '/frontend/css/mihdan-elementor-yandex-maps.css', MIHDAN_ELEMENTOR_YANDEX_MAPS_FILE ), [], MIHDAN_ELEMENTOR_YANDEX_MAPS_VERSION );
 	}
 
 	/**
@@ -122,7 +133,7 @@ class Main {
 	/**
 	 * Create Setting Tab
 	 *
-	 * @param Settings $settings
+	 * @param Settings $settings Elementor "Settings" page in WordPress Dashboard.
 	 *
 	 * @since 1.3
 	 *
@@ -135,7 +146,20 @@ class Main {
 			[
 				'label'    => __( 'Yandex Maps', 'mihdan-elementor-yandex-maps' ),
 				'callback' => function() {
-					echo __( '<p>Go to the <a href="https://developer.tech.yandex.ru/" target="_blank">Developer\'s Dashboard</a> and press “Get key”. In the popup window, select the “JavaScript API, Geocoding API” option.</p><p>After you select the service, the form appears. In this form, you need to provide your contact information. After you fill in the form, the “Service successfully connected” text appears. The created key is now available in the “Keys” section. Use it when you enable the API.</p>', 'mihdan-elementor-yandex-maps' );
+					$message = __( '<p>Go to the <a href="https://developer.tech.yandex.ru/" target="_blank">Developer\'s Dashboard</a> and press “Get key”. In the popup window, select the “JavaScript API, Geocoding API” option.</p><p>After you select the service, the form appears. In this form, you need to provide your contact information. After you fill in the form, the “Service successfully connected” text appears. The created key is now available in the “Keys” section. Use it when you enable the API.</p>', 'mihdan-elementor-yandex-maps' );
+					printf(
+						'%s',
+						wp_kses(
+							$message,
+							array(
+								'p' => array(),
+								'a' => array(
+									'href'   => true,
+									'target' => true,
+								),
+							)
+						)
+					);
 				},
 				'fields'   => [
 					'mihdan_elementor_yandex_maps_key' => [
@@ -171,14 +195,14 @@ class Main {
 	 *
 	 * @access public
 	 */
-	function admin_notice_invalid_api_key() {
-		// translators: %s ссылка на страницу настроек
+	public function admin_notice_invalid_api_key() {
+		// translators: %s ссылка на страницу настроек.
 		$message = sprintf( __( 'To make plugin Mihdan: Elementor Yandex Maps work properly, you should <a href="%s">specify the Yandex Maps API key</a>.', 'mihdan-elementor-yandex-maps' ), admin_url( 'admin.php?page=elementor#tab-' . Settings::TAB_INTEGRATIONS ) );
 
-		echo '<div class="notice notice-warning is-dismissible"><p>' . $message . '</p></div>';
+		echo '<div class="notice notice-warning is-dismissible"><p>' . wp_kses( $message, array( 'a' => array( 'href' => true ) ) ) . '</p></div>';
 	}
 }
 
 Main::instance();
 
-// eof;
+// eof.
